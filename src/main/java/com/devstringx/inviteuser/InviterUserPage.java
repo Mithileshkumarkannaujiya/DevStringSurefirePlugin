b0ad26d9ebb55e944b12ca16a5e584708464f7d6
Mithalesh <mithileshkumar2831999@gmail.com>
2023-07-22 07:38:57 +0530

    sure fire plugin is included in it

---
+++ src/main/java/com/devstringx/inviteuser/InviterUserPage.java
@@ -0,0 +1,36 @@
+package com.devstringx.inviteuser;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.PageFactory;
+
+import com.devstringx.utility.Utility;
+
+public class InviterUserPage extends InviteUserOR {
+	private Utility webUtil;
+String emailId="rajeshkuumarpandey@gmail.com";
+	public InviterUserPage(Utility webUtil) {
+		this.webUtil = webUtil;
+		PageFactory.initElements(webUtil.getDriver(), this);
+	}
+
+	public void inviteUser() {
+		webUtil.click(mangageUsersAndGroups, "Manage Users And Groups");
+		webUtil.click(createBtn, "Create Btn");
+		webUtil.click(inviteUserBtn, " Invite User Btn");
+		webUtil.explicityWait(createBtn, 30);
+		webUtil.sendKey(emailField,  emailId,"  Email field");
+		webUtil.pressEnterKey(emailField);
+		webUtil.click(sendBtn, "Send Btn");
+		
+		
+   
+	}
+
+	public void verifyinvitedUserIsIntheList() {
+		webUtil.click(mangageUsersAndGroups, "Manage Users And Groups");
+		webUtil.validateUserInTheList(listOfusers, emailId ); // iska dusara method banake usmae pass
+																				// karana hai iska true false return
+																				// karake usme use kare
+	}
+
+}
